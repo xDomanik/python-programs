336a20908160eeff0724bc583a47ac4bbe61847e
xDomanikJohnson <Domanik.johnson@icloud.com>
2021-03-13 23:42:17 -0500

    Added code to num guess

--- number-guess.py
+++ number-guess.py
@@ -1,1 +1,15 @@
-print("HELLO")
+'''
+This program lets user set lower & top tier number and select a number between them!
+'''
+import random
+
+#init var
+name = input("Please enter your fullname!\n")
+lower = input(f"{name} Please enter you min number?\n")
+high = input(f"{name} Please enter your max number?\n")
+correctNUm = random.randrange(int(lower), int(high) + 1 )
+
+def userGuess(correctNum):
+
+print(f"{name} {lower} {high}")
+
