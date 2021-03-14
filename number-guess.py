9c1ed7750d59e3c675edf50a786951a2c8bf59bf
xDomanikJohnson <Domanik.johnson@icloud.com>
2021-03-14 13:44:50 -0400

    Added exception to input data

--- number-guess.py
+++ number-guess.py
@@ -5,11 +5,53 @@ import random
 
 #init var
 name = input("Please enter your fullname!\n")
-lower = input(f"{name} Please enter you min number?\n")
-high = input(f"{name} Please enter your max number?\n")
-correctNUm = random.randrange(int(lower), int(high) + 1 )
 
+# Loop to check correct input
+while True:
+
+    try:
+        lower = int(input(f"{name} Please enter you min number?\n"))
+        high = int(input(f"{name} Please enter your max number?\n"))
+    except:
+        print("Numbers only! Try again")
+    else:
+        break
+
+# Gets random number between two ranges: lower & upper
+correctNum =  random.randrange(lower, high + 1 )
+
+# Core Functions
 def userGuess(correctNum):
+    """ Accepts user guess and compares with correct num"""
 
-print(f"{name} {lower} {high}")
+    guess = int(input(f"Input your guess between {lower} and {high}"))
+    
+    print(str(guess) + " this is guess value \n")
 
+    while guess != correctNum:
+        dataString = printMessage(guess, correctNum, name, -1)
+
+        print(dataString + "\n")
+        print(correctNum)
+        userGuess(correctNum)
+        break
+
+    return printMessage(guess, correctNum, name, 1)
+
+def printMessage(userGuess, correctNum, name, modifier):
+    """Print the output message for the user guess on each attempt"""
+    output = ""
+    if(modifier > 0): 
+        output = f"Great! {name} you selected the correct int: {correctNum}"
+        return output
+    
+    # Condition on checking if guess number is too big or too small
+    if userGuess < correctNum:
+        output = f"{name}, Please try again! Your guess is too low\n"
+        return output
+    elif userGuess > correctNum:
+        output = f"{name}, Please try again! your guess is too high\n"
+        return output
+
+
+print(userGuess(correctNum))
